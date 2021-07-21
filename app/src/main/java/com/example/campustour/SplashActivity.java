ff15f9baee7152c0fde9fa504d0bc14b082087dd
seeun-320 <seorang0320@gmail.com>
2021-07-22 03:21:21 +0900

    complete login

--- app/src/main/java/com/example/campustour/SplashActivity.java
+++ app/src/main/java/com/example/campustour/SplashActivity.java
@@ -1,10 +1,10 @@
 package com.example.campustour;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,8 +17,8 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-//                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+//                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
 
                 startActivity(intent);
                 finish();
