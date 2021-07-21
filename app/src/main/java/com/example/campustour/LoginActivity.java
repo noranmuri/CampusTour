3dcb149d503f36929be19a1522f4af20b6b8d28f
seeun-320 <seorang0320@gmail.com>
2021-07-21 21:45:59 +0900

    implement signup(not complete)

--- app/src/main/java/com/example/campustour/LoginActivity.java
+++ app/src/main/java/com/example/campustour/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"로그인하세요", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"회원가입하세요", Toast.LENGTH_LONG).show();
                 Intent signup_page = new Intent(LoginActivity.this,SignupActivity.class);
 
                 startActivity(signup_page);
