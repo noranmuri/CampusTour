796afa836a2ff7266383e5e8bf543e94f9441b77
seeun-320 <seorang0320@gmail.com>
2021-07-21 18:15:49 +0900

    move login to signup

--- app/src/main/java/com/example/campustour/LoginActivity.java
+++ app/src/main/java/com/example/campustour/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.campustour;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView signup = (TextView)findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"로그인하세요", Toast.LENGTH_LONG).show();
+                Intent signup_page = new Intent(LoginActivity.this,SignupActivity.class);
+
+                startActivity(signup_page);
+                finish();
+            }
+
+        });
+
+
     }
 }
