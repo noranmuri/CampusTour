f3ab011fa850d456f761d8bbf796f1607616ad31
thing-zoo <cathy4025@gmail.com>
2021-07-21 14:32:53 +0900

    add button onclick that appear marker with DB(test)

--- app/src/main/java/com/example/campustour/MainActivity.java
+++ app/src/main/java/com/example/campustour/MainActivity.java
@@ -8,20 +8,28 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MainActivity extends FragmentActivity
         implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
-    private int MY_LOCATION_REQUEST_CODE = 1;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +48,6 @@ public class MainActivity extends FragmentActivity
 
         mMap = googleMap;
 
-        LatLng KNU = new LatLng(35.88998331973934, 128.6114154965834);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(KNU);
-        markerOptions.title("경북대학교");
-        markerOptions.snippet("대구캠퍼스");
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KNU, 16));
-
         //권한없으면 부여
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -59,6 +58,15 @@ public class MainActivity extends FragmentActivity
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
+
+//        LatLng KNU = new LatLng(35.88998331973934, 128.6114154965834);
+//
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(KNU);
+//        markerOptions.title("경북대학교");
+//        markerOptions.snippet("대구캠퍼스");
+//        mMap.addMarker(markerOptions);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KNU, 16));
     }
 
     @Override
@@ -68,8 +76,39 @@ public class MainActivity extends FragmentActivity
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
+    }
 
+    public void showBtn(View view) {
 
+        // DB에서 얻은 값 파싱하여
+
+        db.collection("marker")
+                .whereEqualTo("title", "편의점")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("DB", document.getId() + " => " + document.getData());
+
+                                final MarkerOptions marker = new MarkerOptions()
+                                        .position(new LatLng((double)document.get("latitude"), (double)document.get("longitude")))
+                                        .title((String) document.get("title"));
+//                                        .snippet(document.get("snippet"))
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mMap.addMarker(marker);
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.d("DB", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
 
