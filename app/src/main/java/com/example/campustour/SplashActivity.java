78d98adf41819c88ac7e0711200fbd2688d1dd86
eunha812 <0812keh@naver.com>
2021-07-23 00:33:00 +0900

    eunha

--- app/src/main/java/com/example/campustour/SplashActivity.java
+++ app/src/main/java/com/example/campustour/SplashActivity.java
@@ -3,7 +3,12 @@ package com.example.campustour;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,16 +18,28 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-//                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        ImageView splashimage = (ImageView) findViewById(R.id.splashimage);
+        TextView splashtext = (TextView) findViewById(R.id.splashtext);
 
+        Animation animation_image = AnimationUtils.loadAnimation(this, R.anim.splash_image);
+        Animation animation_text = AnimationUtils.loadAnimation(this, R.anim.splash_text);
+        splashimage.startAnimation(animation_image);
+        splashtext.startAnimation(animation_text);
+
+        animation_image.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation){ }
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.splash_in_down, R.anim.splash_out_top);
                 finish();
+                overridePendingTransition(R.anim.splash_out_top, R.anim.splash_in_down);
             }
-        }, 3000);
+        });
+
     }
 }
